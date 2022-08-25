fea944552f21ecf6aa75b881201931094798d35f
Ella <git@ella.cx>
2022-08-25 15:34:20 -0400

    wack

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: [
+      "cloud-f2l2q375z-hack-club-bot.vercel.app",
+      "cloud-fvcuidd4c-hack-club-bot.vercel.app",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
